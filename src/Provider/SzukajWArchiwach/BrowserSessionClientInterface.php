5fb643b1c75c9c242ea2f1f27e76010597a44003
saulgajda <saulgajda@users.noreply.github.com>
2026-09-22 12:16:01 +0200

    feat: add Szukaj w Archiwach browser session runtime

---
+++ src/Provider/SzukajWArchiwach/BrowserSessionClientInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MyTree\ScanProviders\Provider\SzukajWArchiwach;
+
+use MyTree\ScanProviders\Domain\HttpResponse;
+
+interface BrowserSessionClientInterface
+{
+    public function fetchPage(string $url): HttpResponse;
+
+    public function fetchScanImage(string $viewerUrl): HttpResponse;
+}
